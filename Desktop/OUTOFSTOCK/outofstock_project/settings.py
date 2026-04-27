1a985221c2e3e23950ae4171ca09f7c5b6765d97
miricodefr <miricodefr@gmail.com>
2026-04-27 13:36:08 +0300

    add whitenoise storage

--- Desktop/OUTOFSTOCK/outofstock_project/settings.py
+++ Desktop/OUTOFSTOCK/outofstock_project/settings.py
@@ -81,3 +81,4 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
